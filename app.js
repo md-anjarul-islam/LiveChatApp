a9cb5afeecf68066df3fe6e948c555bea721bc3d
Anjarul <anjarulrobin@gmail.com>
2020-03-09 14:12:49 +0600

    Add some API and send message at real time
    -- Add API for message interchange
    -- Send message to all the registered users on real time
    -- Using socket ID to send message to all the users

--- app.js
+++ app.js
@@ -1,33 +1,63 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const path = require('path');
-const allMessage = [];
+const bodyParser = require("body-parser");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const path = require("path");
 
-app.use(express.static(path.join(__dirname,'public')));
+const messages = [];
+const users = [];
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/messages", (req, res) => {
+  res.json(messages);
 });
 
-let user=0;
-
-io.on("connection", (socket)=>{
-    console.log('a user connected', socket.id);
-    user++;
-    socket.emit(`user1`, "user"+user);
-    
-    socket.on("newMessage", function(message){      /// recieved a new message
-        allMessage.push(message);
-        console.log(allMessage);
-        io.emit("updateMessage", allMessage);   // save in database and then send back all message
-    })
-
-    socket.on("disconnect", ()=>{
-        console.log("a user disconnected");        
-    });
-
+app.post("/api/messages", (req, res) => {
+  const newMessage = req.body;
+  messages.push(newMessage);
+  messages.forEach(message => {
+    if (message.socketId != newMessage.socketId)
+      io.to(message.socketId).emit("updateMessage", messages);
+  });
+  res.json(messages);
 });
 
-server.listen(3000, ()=> console.log("Listening on port 3000"));
+app.post("/api/login", function(req, res) {
+  let user = req.body;
+  users.push(user);
+  delete user.password;
+  messages.push({
+    from: user.name,
+    message: "Hi, I am a new uesr.",
+    socketId: user.socketId
+  });
+
+  messages.forEach(message => {
+      io.to(message.socketId).emit("updateMessage", messages);
+  });
+
+  res.json(user);
+});
+
+io.on("connection", socket => {
+  console.log("a user connected", socket.id);
+
+  socket.on("newMessage", function(message) {
+    /// recieved a new message
+    messages.push(message);
+    console.log(messages);
+    socket.emit("updateMessage", messages); // save in database and then send back all message
+  });
+  socket.on("sentMessage", function(data) {});
+
+  socket.on("disconnect", () => {
+    // users.filter( (user) => user.socketId !== socket.id )
+    console.log("a user disconnected");
+  });
+});
+
+server.listen(3000, () => console.log("Listening on port 3000"));
