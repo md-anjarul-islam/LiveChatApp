7c079b5fa0d22b69f999dee75a4c7f7de60dc204
Anjarul <anjarulrobin@gmail.com>
2020-03-09 18:57:09 +0600

    Configure the app for different routing

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,1 +1,17 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngRoute"]);
+
+app.config( function($routeProvider){
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'html/loginView.html',
+            controller: 'chatController'
+        })
+        .when('/chatroom', {
+            templateUrl: 'html/chatView.html',
+            controller: 'chatController'
+        })
+        .otherwise({
+            redirectTo: '/',
+            controller: 'chatController'
+        })
+})
