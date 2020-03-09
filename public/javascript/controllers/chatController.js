bc06d43a1d963f23de8d245ff9ae53d263a98305
Anjarul <anjarulrobin@gmail.com>
2020-03-09 14:24:15 +0600

    Add feature to chat controller
    -- Call API from controller
    -- Place data recieved from API to the html page
    -- Add login function for user login

--- public/javascript/controllers/chatController.js
+++ public/javascript/controllers/chatController.js
@@ -1,17 +1,49 @@
-app.controller("chatController", function($scope, $http, socketService) {
-    
-    $scope.sendMessage = function(msg) {
-        messageDetails = {
-            from: $scope.name,
-            message: $scope.message
-        }
-      socketService.sendMessage(messageDetails);
-    };
-    // socketService.on();
+app.controller("chatController", function($scope, $http) {
+  var socket = io();
 
-    socket.on("updateMessage", function(messages) {
-      $scope.$apply(function() {
-        $scope.allMessage = messages;
+  $scope.login = function() {
+    let user = {
+      name: $scope.name,
+      password: $scope.password,
+      socketId: socket.id
+    };
+
+    $http({
+      method: "POST",
+      url: "/api/login",
+      data: JSON.stringify(user)
+    })
+      .then(response => {
+        $scope.user = response.data;
+      })
+      .catch(err => {
+        $scope.errorMessage = err.message;
       });
+  };
+
+  socket.on("updateMessage", function(updatedMessage) {
+    $scope.$apply(function() {
+      $scope.allMessage = updatedMessage;
     });
   });
+
+  $scope.sendMessage = function() {
+    let messageDetails = {
+      from: $scope.user.name,
+      message: $scope.message,
+      socketId: socket.id
+    };
+
+    $http({
+      method: "POST",
+      url: "/api/messages",
+      data: messageDetails
+    })
+      .then(response => {
+        $scope.allMessage = response.data;
+      })
+      .catch(err => {
+        alert(err.message);
+      });
+  };
+});
