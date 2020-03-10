d4d8aee54e9e67b302792b5635333e89aae4059e
Anjarul <anjarulrobin@gmail.com>
2020-03-10 14:52:03 +0600

    Add login, registration and update user schema
    -- Add login function which updates the socket id of the logged in user
    -- Add function to find all the users so that the new user can get all
    of them
    -- Add findUser function to find a specific user

--- model/user.js
+++ model/user.js
@@ -1,30 +1,56 @@
 const db = require("../config/db");
 
 const userSchema = new db.Schema({
-  username: String,
+  _id: Number,
+  name: String,
   password: String,
- });
+  socketId: String
+});
 
 const User = new db.model("Users", userSchema);
 
-async function createUser(aUser) {
+async function createUser(aUser, socketId) {
   try {
+    aUser._id = Math.round(Math.random() * 100000000);
+    aUser.socketId = socketId;
     const newUser = new User(aUser);
     return await newUser.save();
   } catch (err) {
+    console.log(err);
     return err;
   }
 }
 
-async function findUser(userInfo) {
+async function login(user, socketId){
   try{
-    return await User.findOne(userInfo).select({ password: 0 });    
+    return await User.updateOne({_id: user._id}, {$set: {socketId: socketId}});
   }catch(err){
+    console.log(err);
+    return null;
+  }
+}
+
+async function findUser(userInfo) {
+  try {
+    return await User.findOne(userInfo).select({ password: 0 });
+  } catch (err) {
+    console.log(err);
     return err;
+  }
+}
+
+async function findAllUser() {
+  try {
+    return await User.find().select({ password: 0 });
+  } catch (err) {
+    console.log(err);
+    return null;
   }
 }
 
 module.exports = {
   createUser,
-  findUser 
+  findUser,
+  findAllUser,
+  login
 };
