e4555d7f23853ff6ea9c8eb0053c9e0228bbaa3c
Anjarul <anjarulrobin@gmail.com>
2020-03-10 20:23:34 +0600

    Move login, register function to userController
    -- move login and registration function to user controller
    -- use socket from the rootScope to avoid creating socket again
    while changing route-view

--- public/javascript/controllers/chatController.js
+++ public/javascript/controllers/chatController.js
@@ -1,56 +1,19 @@
-app.controller("chatController", function($scope, $rootScope, $http) {
-  var socket = io();
-
-  $scope.register = function(){
-    let user = {
-      name: $scope.name,
-      password: $scope.password
-    }
-    let socketId = socket.id;
-    let sendData = {user, socketId};
-
-    $http({
-      method: "POST",
-      url: "api/register",
-      data: JSON.stringify(sendData)
-    }).then( (response) =>{
-      console.log(response.data);
-      $scope.userInfo = response.data;
-    }).catch(err=>console.log(err));
-  }
-
-  $scope.login = function() {
-    let user = {
-      _id: 21637996,
-      name: $scope.name,
-      password: $scope.password,
-    };
-    let socketId = socket.id;
-    let sendData = {user, socketId};
-
-    $http({
-      method: "POST",
-      url: "/api/login",
-      data: JSON.stringify(sendData)
-    })
-      .then(response => {
-        $scope.user = response.data;
-        console.log(response.data);
-        $rootScope.link1 = "#!/";
-        $rootScope.link2 = "#!/chatroom";
-        $rootScope.link3 = "#!/logout";
-        $rootScope.linkName1 = "Home";
-        $rootScope.linkName2 = "Chat Room";
-        $rootScope.linkName3 = "Log Out";
-      })
-      .catch(err => {
-        $scope.errorMessage = err.message;
-      });
-  };
-
+app.controller("chatController", function($scope, $rootScope, $http, $location, userService) {
+  var socket = $rootScope.socket;
+ 
   socket.on("updateMessage", function(updatedMessage) {
+    console.log("updatedMessage", updatedMessage);
     $scope.$apply(function() {
-      $scope.allMessage = updatedMessage;
+      userService.allMessage = updatedMessage;
+    });
+  });
+
+  socket.on("updateUser", function(updatedUser) {
+    console.log("updatedUser", updatedUser);
+    $scope.$apply(function() {
+      userService.setAllUser(updatedUser);
+      $scope.allUser = userService.getAllUser();
+      $scope.user = userService.getUser();
     });
   });
 
