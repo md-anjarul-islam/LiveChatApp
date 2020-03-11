d706030bdb10b6cdd3c06f90782f2b9812b6f282
Anjarul <anjarulrobin@gmail.com>
2020-03-11 20:22:03 +0600

    Add some new properties
    -- Get/Set the current user
    -- Get/Set all the registered user
    -- Get/Set all the messages between two user
    -- Init/Push/Get messages between two user

--- public/javascript/services/userService.js
+++ public/javascript/services/userService.js
@@ -1,21 +1,46 @@
 app.factory("userService", function() {
   var obj = {};
+  obj.messages = [];
 
   obj.setUser = function(user) {
-    obj.user = user;
+    obj.currentUser = user;
   };
 
   obj.getUser = function() {
-    return obj.user;
+    return obj.currentUser;
   };
 
   obj.setAllUser = function(allUser) {
     obj.allUser = allUser;
+    obj.users = {};
+    // obj.allUser is also an object which is a key-value pair.
+    // the key here is the userId and the value is the user itself
+    let userId = null;
+    allUser.forEach( user => {
+      userId = user._id;
+      obj.users[userId] = user;
+    });
   };
 
   obj.getAllUser = function() {
     return obj.allUser;
   };
+
+  obj.getUserById = function(userId){
+    return obj.users[userId];
+  }
+
+  obj.initMessage = function(messages){
+    obj.messages = messages;
+  }
+  
+  obj.pushMessage = function(newMessage){
+    obj.messages.push(newMessage);
+  }
+
+  obj.getMessages = function(){
+    return obj.messages;
+  }
 
   return obj;
 });
