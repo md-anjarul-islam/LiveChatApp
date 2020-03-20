9da95eed1914234a86cb47f80ede012ec640c9a7
Anjarul <anjarulrobin@gmail.com>
2020-03-20 10:17:18 +0600

    Add auth middleware
    - To validate user authentication token

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+const userHandler = require("../model/user");
+
+module.exports = async function(req, res, next) {
+  let token = req.headers.authtoken;
+  let verifiedToken = userHandler.verifyToken(token);
+    
+  if (!verifiedToken) res.status(401).json({ message: "Unauthorized Access!" });
+  else 
+      req.headers.user = verifiedToken;
+      next();
+};
