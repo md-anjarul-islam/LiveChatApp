088c36dfe34dcc77f49b53f868fe7b2cb4baa460
Anjarul <anjarulrobin@gmail.com>
2020-03-25 11:20:19 +0600

    Fix a small bug in async/await

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,8 @@ const userHandler = require("../model/user");
 
 module.exports = async function(req, res, next) {
   let token = req.headers.authtoken;
-  let verifiedToken = userHandler.verifyToken(token);
-    
+  let verifiedToken = await userHandler.verifyToken(token);
+      
   if (!verifiedToken) res.status(401).json({ message: "Unauthorized Access!" });
   else {
       req.headers.user = verifiedToken;
