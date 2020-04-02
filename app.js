175eec36f90b33f437ce148ac3220d9e0e2c5218
Anjarul <anjarulrobin@gmail.com>
2020-04-02 11:34:53 +0600

    Decentralize the logic and routing parts
    - Move the routing logic into routes/ folder
    - Set the socket io instance to the global object

--- app.js
+++ app.js
@@ -2,95 +2,28 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
 const path = require("path");
-const userHandler = require("./model/user");
-const messageHandler = require("./model/message");
-const authProvider = require('./middleware/auth');
+const userRoute = require("./routes/user");
+const messageRoute = require("./routes/message");
+const authRoute = require("./routes/auth");
+global.SOCKET_IO = require("socket.io")(server);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.post("/api/register", async function(req, res) {
-  let newUser = await userHandler.createUser(req.body.user, req.body.socketId);
-  let allUser = await userHandler.findAllUser();
-  allUser.forEach(user => {
-    io.to(user.socketId).emit("updateUser", newUser);
-  });
-  let token = newUser.getAuthToken();
-  res.set({"x-token":token}).json(newUser);
-});
-
-app.post("/api/login", async function(req, res) {
-  let loggedUser = await userHandler.login(req.body.user, req.body.socketId);
-  loggedUser = await userHandler.findUser(req.body.user);
-
-  if (loggedUser) {
-    let allUser = await userHandler.findAllUser();
-    allUser.forEach(user => {
-    console.log("emitting to ",user.name);    
-
-      io.to(user.socketId).emit("updateUser", allUser);
-    });
-    let token = loggedUser.getAuthToken();
-    res.set({"x-token":token}).json(loggedUser);
-  } else {
-    res.status(401).json(loggedUser);
-  }
-});
-
-app.get("/api/users", async function(req, res) {
-  let allUser = await userHandler.findAllUser();
-  res.json(allUser);
-});
-
-app.get("/api/users/profile", authProvider, async function(req, res){
-  let user = req.headers.user;
-  try{
-    profile = await userHandler.findUser({_id: user._id});
-    res.json(profile);
-  }catch(err){
-    res.status(404).json({message: "Unauthorized Access!"})
-  }
-});
-
-
-
-
-app.get("/api/users/:userId/messages", authProvider, async (req, res) => {
-  let id1 = req.headers.user._id;
-  let id2 = req.params.userId;
-  try{
-    const messages = await messageHandler.getMessage(id1, id2);
-    res.json(messages);
-  }catch(err){
-    res.json({message: "Error!"});
-  }
-});
-
-app.post("/api/users/:userId/messages", authProvider, async (req, res) => {
-  let newMessage = req.body.message;
-  let id1 = req.headers.user._id;
-  let id2 = req.params.userId;
-
-  try{
-    newMessage = await messageHandler.createMessage(id1, id2, newMessage);
-    user2 = await userHandler.findUser({_id: id2});
-    io.to(user2.socketId).emit("updateMessage", newMessage);
-    res.json(newMessage);
-  }catch(err){
-    res.json({message: "Error!"});
-  }
-});
-
-
+/* 
 io.on("connection", socket => {
+  global.CLIENT_SOCKET = socket;
+  global.SOCKET_IO = io;
   console.log("a user connected", socket.id);
-
   socket.on("disconnect", () => {
     console.log("a user disconnected");
   });
-});
+}); */
 
-server.listen(3000, () => console.log("Listening on port 3000"));
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/messages', messageRoute);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Listening on port", PORT));
