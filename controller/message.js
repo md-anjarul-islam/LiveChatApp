79a00ce5718ae0bd1e54551025f829edabd2b27d
Anjarul <anjarulrobin@gmail.com>
2020-04-02 21:14:24 +0600

    Fix some bugs
    - Add missing userHandler require
    - Send error status code when error occurs

--- controller/message.js
+++ controller/message.js
@@ -1,15 +1,15 @@
 const messageHandler = require('../model/message');
+const userHandler = require('../model/user');
 const notificationService = require('../util/notification')
 
 async function getMessage(req, res) {
     let id1 = req.headers.user._id;
     let id2 = req.params.userId;
-    console.log(id1, id2);
     try{
       const messages = await messageHandler.getMessage(id1, id2);
       res.json(messages);
     }catch(err){
-      res.json({message: "Error!"});
+      res.status(400).json({message: "Error!"});
     }
   };
 
@@ -20,11 +20,12 @@ async function postMessage (req, res){
   
     try{
       newMessage = await messageHandler.createMessage(id1, id2, newMessage);
-      user2 = await userHandler.findUser({_id: id2});
+      let user2 = await userHandler.findUser({_id: id2});
       notificationService.notifyToUser(user2.socketId,"updateMessage", newMessage);
       res.json(newMessage);
     }catch(err){
-      res.json({message: "Error!"});
+      console.log(err);
+      res.status(400).json({message: "Error!"});
     }
   };
 
