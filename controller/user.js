d7cc3247f6e5a6634b984f88d315f98cc0ad5d9f
Anjarul <anjarulrobin@gmail.com>
2020-04-02 12:01:46 +0600

    Provide user related functions
    - User related infromation like profile info
    - Update profile is also here in this controller
    - This controller also provides function to get the other users

--- controller/user.js
+++ controller/user.js
@@ -1,1 +1,30 @@
-const router = require('express').Router();
+const userHandler = require('../model/user');
+
+async function getProfile(req, res){
+    let user = req.headers.user;
+    try{
+      profile = await userHandler.findUser({_id: user._id});
+      res.json(profile);
+    }catch(err){
+      res.status(404).json({message: "Unauthorized Access!"})
+    }
+  }
+  
+  async function updateProfile(req, res){
+
+  }
+
+  async function getAllUser(req, res){
+    try{
+      let allUser = await userHandler.findAllUser();
+      res.json(allUser);
+    } catch(err){
+      res.status(400).json({message: "Error getting data!"});
+    }
+  }
+  
+  module.exports = {
+      getProfile,
+      updateProfile,
+      getAllUser
+  }
