b9fe3b0d604d32b547378d2dd817baeb2cf2082e
Anjarul <anjarulrobin@gmail.com>
2020-04-02 12:08:45 +0600

    Develop code structure
    - write less code
    - improve code quality

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,68 +1,64 @@
 var app = angular.module("app", ["ngRoute", "ngCookies"]);
 
-app.config( function($routeProvider){
+app
+  .config(function($routeProvider) {
     window.routes = {
-        "/": {
-            controller: 'userController',
-            requireLogin: false
-        },
-        '/register': {
-            templateUrl: 'views/registerView.html',
-            controller: 'userController',
-            requireLogin: false
-        },
-        '/login': {
-            templateUrl: 'views/loginView.html',
-            controller: 'userController',
-            requireLogin: false
-        },
-        '/chatroom': {
-            templateUrl: 'views/chatView.html',
-            redirect: '/login',
-            controller: 'chatController',
-            requireLogin: true
-        },
-        '/profile': {
-            template: '<h1> Profile Page </h1>',
-            redirect: '/login',
-            requireLogin: true
-        },
-        '/logout': {
-            template: '<h1>This is Logout page </h1>',
-            requireLogin: true,
-            redirect: "/login"
-        }
+      "/": {
+        controller: "userController",
+        requireLogin: false
+      },
+      "/register": {
+        templateUrl: "views/registerView.html",
+        controller: "userController",
+        requireLogin: false,
+        hideFromUser: true
+      },
+      "/login": {
+        templateUrl: "views/loginView.html",
+        controller: "userController",
+        requireLogin: false,
+        hideFromUser: true
+      },
+      "/chatroom": {
+        templateUrl: "views/chatView.html",
+        redirect: "/login",
+        controller: "chatController",
+        requireLogin: true
+      },
+      "/profile": {
+        template: "<h1> Profile Page </h1>",
+        redirect: "/login",
+        requireLogin: true
+      },
+      "/logout": {
+        template: "<h1>This is Logout page </h1>",
+        requireLogin: true,
+        redirect: "/login"
+      }
     };
-    
-    for(path in window.routes){
-        $routeProvider.when(path, window.routes[path]);
+
+    for (path in window.routes) {
+      $routeProvider.when(path, window.routes[path]);
     }
 
     $routeProvider.otherwise({
-        redirectTo: '/',
-        controller: 'userController'
-    })
-}).run(function($http, $rootScope, $location, sessionService, AuthService){    
-    $rootScope.$on("$locationChangeStart", 
-    function(event, next, current){
-       
-        if( !!sessionService.getAuthToken() ){
-            if(next.indexOf("login") != -1 || next.indexOf("register") != -1){
-                $location.path("/");
-                return;
-            }
-        }
-
-        for(let route in window.routes){
-            if(next.indexOf(route) !=-1 ){
-                if(window.routes[route].requireLogin && !sessionService.getAuthToken()){
-                    AuthService.memorize(route);
-                    let nextPath = window.routes[route].redirect || "/login";
-                    $location.path(nextPath);
-                    break;
-                }                
-            }
-        }
-        console.log("event triggered", next, current);
+      redirectTo: "/",
+      controller: "userController"
     });
-})
+  })
+  .run(function($http, $rootScope, $location, sessionService, AuthService) {
+    $rootScope.$on("$routeChangeStart", function(event, next, current) {
+      let routePath = next.$$route.originalPath;
+
+      if ( !!sessionService.getAuthToken() && window.routes[routePath].hideFromUser == true ) {
+          $location.path("/");
+          return;
+      }
+
+      if (window.routes[routePath].requireLogin && !sessionService.getAuthToken()) {
+        AuthService.memorize(routePath);
+        let nextPath = window.routes[routePath].redirect || "/login";
+        $location.path(nextPath);
+      }
+    });
+  });
